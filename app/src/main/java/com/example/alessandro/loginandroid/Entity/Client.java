0140f45d02fd7b7932fa59cb662926c163db76b2
jonas <sjnao89@gmail.com>
2015-11-10 08:42:13 +0100

    commenti + TODO

--- app/src/main/java/com/example/alessandro/loginandroid/Entity/Client.java
+++ app/src/main/java/com/example/alessandro/loginandroid/Entity/Client.java
@@ -2,6 +2,8 @@ package com.example.alessandro.loginandroid.Entity;
 
 import android.os.AsyncTask;
 
+import com.google.gson.Gson;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -17,11 +19,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
+/**
+ * Classe che implementa l Utente compresi i dati dei Token
+ */
 public class Client {
-    User currentUser;
-    String accessToken, refreshToken, RANDOM_ID, SECRET_ID, GRANT_TYPES;
+    User currentUser; // dati dell utente
+    String accessToken, refreshToken, RANDOM_ID, SECRET_ID, GRANT_TYPES; // Dati del Client + Token
 
+    /**
+     * Costruttore, i RANDOM_ID e SECRET_ID non servono in quanto sono costanti
+     * @param currentUser
+     * @param accessToken
+     * @param refreshToken
+     * @param GRANT_TYPES
+     */
     public Client(User currentUser, String accessToken, String refreshToken, String GRANT_TYPES) {
         this.currentUser = currentUser;
         this.accessToken = accessToken;
@@ -31,12 +42,23 @@ public class Client {
         this.GRANT_TYPES = GRANT_TYPES;
     }
 
+    /**
+     * Costruttore vuoto
+     */
     public Client(){}
 
+
+    /**
+     * Metodo per il Login/ovvero sparare a /authorization
+     */
     public void login(){
         new LoginTask(this).execute();
     }
 
+
+    /**
+     * Task che in Background manda un post ad /authotization
+     */
     public class LoginTask extends AsyncTask<String, Void, Void>{
 
         private Client client;
@@ -49,33 +71,31 @@ public class Client {
             try {
 
                 HttpClient httpclient = new DefaultHttpClient();
+                // sito a cui fare il post
                 HttpPost httppost = new HttpPost("http://10.0.2.2:4567/authorization");
+                // Lista di 5 valori che mandiamo
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(5);
+                // Valori:
                 nameValuePairs.add(new BasicNameValuePair("random_id", client.RANDOM_ID));
                 nameValuePairs.add(new BasicNameValuePair("secret_id", client.SECRET_ID));
                 nameValuePairs.add(new BasicNameValuePair("grant_types", client.GRANT_TYPES));
                 nameValuePairs.add(new BasicNameValuePair("email", client.currentUser.getEmail()));
                 nameValuePairs.add(new BasicNameValuePair("password", client.currentUser.getEmail()));
-
+                //Mettere la lista nel post, cosi da poterla mandare
+                // prima l aveva solo creata ma non settata come da mandare
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                // Settiamo l Header perché il sito si aspetta un header Parametro di tipo Authorization
+                httppost.setHeader("Authorization", client.refreshToken);
 
-                httppost.setHeader("Authorization", client.accessToken);
-                //httppost.setHeader("Content-type", "application/json");
-                //httppost.setHeader("Accept","application/json");
-
-                // Execute HTTP Post Request
-//                HttpResponse response = null;
-//
-//                response = httpclient.execute(httppost);
+                // con l execute, mandiamo il post
                 HttpResponse response = httpclient.execute(httppost);
+                // prendiamo la risposta del server e lo salviamo come stringa in "json"
                 HttpEntity entity = response.getEntity();
                 String json = EntityUtils.toString(entity);
-                JSONObject jsonObject = new JSONObject(json);
-                //jsonObject.getString("access_Token");
-                jsonObject.getString("type");
-
-                //Response responseServer = new Gson().fromJson(jsonObject);
-                System.out.println(json);
+                // Creiamo una classe di tipo Response grazie alla risposta json
+                Response responseServer = new Gson().fromJson(json, Response.class);
+                // Metodo per gestire la risposta ottenuta
+                handleResponse(responseServer);
 
             }
             catch (Exception e){
@@ -83,6 +103,20 @@ public class Client {
                 e.printStackTrace();
             }
             return null;
+        }
+    }
+
+    /**
+     * Gestisce la risposta del Server
+     * @param responseServer
+     */
+    private void handleResponse(Response responseServer) {
+        switch (responseServer.getType()){
+            case "2":
+                this.accessToken = responseServer.getAccess_Token();
+                this.refreshToken = responseServer.getRefresh_Token();
+                break;
+            //TODO: definiamo e creaimo altri casi.Non solo per il login, ma anche per altri post fatti.
         }
     }
 }
