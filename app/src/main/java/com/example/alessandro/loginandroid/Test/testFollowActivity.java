9549c2fb2fecbae469905bafdc2391ca4a1f43b3
blennoit <alessandro.distefano1@live.it>
2015-12-10 15:54:34 +0100

    new search

--- app/src/main/java/com/example/alessandro/loginandroid/Test/testFollowActivity.java
+++ app/src/main/java/com/example/alessandro/loginandroid/Test/testFollowActivity.java
@@ -2,19 +2,44 @@ package com.example.alessandro.loginandroid.Test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.example.alessandro.loginandroid.Activity.ListUser;
+import com.example.alessandro.loginandroid.Entity.ClientLocalStore;
+import com.example.alessandro.loginandroid.Entity.User;
 import com.example.alessandro.loginandroid.R;
+import com.google.gson.Gson;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class testFollowActivity extends Activity implements View.OnClickListener {
     ImageButton buttonHOME, buttonSEARCH, buttonFOLLOW, buttonPROFILE;
 
     private Button followContactButton;
     private Button followMessageButton;
+    ArrayList<User> users;
+    ClientLocalStore clientLocalStore;
 
     private ListView followListView;
 
@@ -38,6 +63,14 @@ public class testFollowActivity extends Activity implements View.OnClickListener
 
         followContactButton.setOnClickListener(this);
         followMessageButton.setOnClickListener(this);
+        followListView = (ListView)findViewById(R.id.followListView);
+
+        users = new ArrayList<>();
+
+        clientLocalStore = new ClientLocalStore(this);
+        new GetFolloweedTask(clientLocalStore.getUser()).execute();
+        ListUser adapter = new ListUser(this, users);
+        followListView.setAdapter(adapter);
 
     }
 
@@ -67,5 +100,36 @@ public class testFollowActivity extends Activity implements View.OnClickListener
                 break;
         }
 
+    }
+
+    public class GetFolloweedTask extends AsyncTask<Void,Void,Void>{
+        private User user;
+        public GetFolloweedTask(User user){this.user=user;}
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost("http://10.0.2.2:4567/getFollowed");
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                nameValuePairs.add(new BasicNameValuePair("id_user", Integer.toString(user.getId_user())));
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity entity =response.getEntity();
+                String json = EntityUtils.toString(entity);
+                JSONArray usersArray = new JSONArray(json);
+                System.out.println(json);
+
+                for (int i = 0; i < usersArray.length(); i++) {
+
+                    User user = new Gson().fromJson(usersArray.get(i).toString(), User.class);
+                    users.add(user);
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/alessandro/loginandroid/Test/testSearchActivity.java
+++ app/src/main/java/com/example/alessandro/loginandroid/Test/testSearchActivity.java
@@ -2,15 +2,40 @@ package com.example.alessandro.loginandroid.Test;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.example.alessandro.loginandroid.R;
 
 public class testSearchActivity extends Activity {
 
+    private AutoCompleteTextView searchRoleAutoComplete;
+    String[] roles = {"personal trainer", "pensionato", "cane", "gatto","paramedito","perdio","provaprova"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_search);
+
+        searchRoleAutoComplete = (AutoCompleteTextView)findViewById(R.id.searchRoleAutoComplete);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_dropdown_item_1line, roles);
+        searchRoleAutoComplete.setAdapter(adapter);
+        searchRoleAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+                String selected = (String) adapter.getItemAtPosition(pos);
+                Toast.makeText(
+                        getApplicationContext(),
+                        "hai selezionato " + selected,
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        });
+
     }
 
 }
